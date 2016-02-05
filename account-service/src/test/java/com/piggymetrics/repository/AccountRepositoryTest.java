7c6a64d88d4049f23dcaca6c7c40eaa6a7102f10
sqshq <sqshq@sqshq.com>
2016-02-06 01:34:58 +0300

    embedded mongodb test configuration

--- account-service/src/test/java/com/piggymetrics/repository/AccountRepositoryTest.java
+++ account-service/src/test/java/com/piggymetrics/repository/AccountRepositoryTest.java
@@ -1,16 +1,15 @@
 package com.piggymetrics.repository;
 
 import com.piggymetrics.AccountApplication;
+import com.piggymetrics.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AccountApplication.class)
-@WebAppConfiguration
 public class AccountRepositoryTest {
 
 	@Autowired
@@ -18,7 +17,13 @@ public class AccountRepositoryTest {
 
 	@Test
 	public void shouldRetrieveAccountByDemoUser() {
-//		Account account = repository.findByUserUsername("demo");
-//		assertNotNull(account);
+
+		Account account = new Account();
+		account.setName("demo");
+		account.setNote("my note!");
+		repository.save(account);
+
+		Account found = repository.findByName("demo");
+		System.out.println(found);
 	}
 }
