a719c1a38e501182fef4e794a0e62e7a3ae5ab5e
sqshq <sqshq@sqshq.com>
2016-02-24 02:55:15 +0300

    demo account dump extension

--- mongodb/account-service-dump.js
+++ mongodb/account-service-dump.js
@@ -5,38 +5,87 @@
 print('dump start');
 
 db.accounts.update(
-   { "_id": "demo" },
-   {
-   "_id": "demo",
-   "lastSeen": new Date(),
-   "note": "demo note",
-   "expenses": [
-      {
-         "amount": 10,
-         "currency": "USD",
-         "icon": "smoking",
-         "period": "DAY",
-         "title": "Grocery"
-      }
-   ],
-   "incomes": [
-      {
-         "amount": 9100,
-         "currency": "USD",
-         "icon": "wallet",
-         "period": "MONTH",
-         "title": "Salary"
-      }
-   ],
-   "saving": {
-      "amount": 1500,
-      "capitalization": false,
-      "currency": "USD",
-      "deposit": true,
-      "interest": 3.32
-   }
-   },
-   { upsert: true }
+    { "_id": "demo" },
+    {
+    "_id": "demo",
+    "lastSeen": new Date(),
+    "note": "demo note",
+    "expenses": [
+        {
+            "amount": 1300,
+            "currency": "USD",
+            "icon": "home",
+            "period": "MONTH",
+            "title": "Rent"
+        },
+        {
+            "amount": 120,
+            "currency": "USD",
+            "icon": "utilities",
+            "period": "MONTH",
+            "title": "Utilities"
+        },
+        {
+            "amount": 20,
+            "currency": "USD",
+            "icon": "meal",
+            "period": "DAY",
+            "title": "Meal"
+        },
+        {
+            "amount": 240,
+            "currency": "USD",
+            "icon": "gas",
+            "period": "MONTH",
+            "title": "Gas"
+        },
+        {
+            "amount": 3500,
+            "currency": "EUR",
+            "icon": "island",
+            "period": "YEAR",
+            "title": "Vacation"
+        },
+        {
+            "amount": 30,
+            "currency": "EUR",
+            "icon": "phone",
+            "period": "MONTH",
+            "title": "Phone"
+        },
+        {
+            "amount": 700,
+            "currency": "USD",
+            "icon": "sport",
+            "period": "YEAR",
+            "title": "Gym"
+        }
+    ],
+    "incomes": [
+        {
+            "amount": 42000,
+            "currency": "USD",
+            "icon": "wallet",
+            "period": "YEAR",
+            "title": "Salary"
+        },
+        {
+            "amount": 500,
+            "currency": "USD",
+            "icon": "edu",
+            "period": "MONTH",
+            "title": "Scholarship"
+        }
+    ],
+    "saving": {
+            "amount": 5900,
+            "capitalization": false,
+            "currency": "USD",
+            "deposit": true,
+            "interest": 3.32
+        }
+    },
+    { upsert: true }
 );
 
 print('dump complete');
