f039a41939019b6f738f2978c3a559e91b357787
sqshq <sqshq@sqshq.com>
2016-04-01 03:18:56 +0300

    remove useless annotation

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -2,12 +2,10 @@ package com.piggymetrics.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableTurbineStream
 @EnableHystrixDashboard
 public class MonitoringApplication {
