aa536d888b4c76ffc623592187110d2f500137b1
Chi Dov <d.chiproeng@gmail.com>
2018-07-16 01:01:17 -0500

    separate config from Statistic Application for loose coupling unit test purpose

--- statistics-service/src/main/java/com/piggymetrics/statistics/StatisticsApplication.java
+++ statistics-service/src/main/java/com/piggymetrics/statistics/StatisticsApplication.java
@@ -8,35 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@EnableResourceServer
 @EnableDiscoveryClient
 @EnableOAuth2Client
 @EnableFeignClients
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StatisticsApplication {
 
-	@Autowired
-	private ResourceServerProperties sso;
-
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticsApplication.class, args);
-	}
-
-	@Bean
-	public ResourceServerTokenServices tokenServices() {
-		return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
 	}
 
 	@Configuration
