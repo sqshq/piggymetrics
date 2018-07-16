33fafc7df10b976cd534032e3cf48d5e9ccdd2cd
Chi Dov <d.chiproeng@gmail.com>
2018-07-16 01:38:09 -0500

    fix openfiegn import for accountServiceClient

--- notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
+++ notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
@@ -1,6 +1,6 @@
 package com.piggymetrics.notification.client;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
