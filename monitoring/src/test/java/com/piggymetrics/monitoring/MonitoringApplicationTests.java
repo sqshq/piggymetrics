fc69832ba56f904b29ca60e8d351528310f2b2e0
Chi Dov <d.chiproeng@gmail.com>
2018-07-16 01:18:53 -0500

    update monitor server to spring boot 2.0.3 and spring cloud finchley

--- monitoring/src/test/java/com/piggymetrics/monitoring/MonitoringApplicationTests.java
+++ monitoring/src/test/java/com/piggymetrics/monitoring/MonitoringApplicationTests.java
@@ -2,11 +2,11 @@ package com.piggymetrics.monitoring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MonitoringApplication.class)
+@SpringBootTest
 public class MonitoringApplicationTests {
 
 	@Test
