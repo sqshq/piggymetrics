3ecad94e24f410970ec01f27a2c42ffad2bd081e
Chi Dov <d.chiproeng@gmail.com>
2018-07-16 01:39:07 -0500

    add single quote for RecipientRepository

--- notification-service/src/main/java/com/piggymetrics/notification/repository/RecipientRepository.java
+++ notification-service/src/main/java/com/piggymetrics/notification/repository/RecipientRepository.java
@@ -12,11 +12,11 @@ public interface RecipientRepository extends CrudRepository<Recipient, String> {
 
 	Recipient findByAccountName(String name);
 
-	@Query("{ $and: [ {scheduledNotifications.BACKUP.active: true }, { $where: 'this.scheduledNotifications.BACKUP.lastNotified < " +
+	@Query("{ $and: [ {'scheduledNotifications.BACKUP.active': true }, { $where: 'this.scheduledNotifications.BACKUP.lastNotified < " +
 			"new Date(new Date().setDate(new Date().getDate() - this.scheduledNotifications.BACKUP.frequency ))' }] }")
 	List<Recipient> findReadyForBackup();
 
-	@Query("{ $and: [ {scheduledNotifications.REMIND.active: true }, { $where: 'this.scheduledNotifications.REMIND.lastNotified < " +
+	@Query("{ $and: [ {'scheduledNotifications.REMIND.active': true }, { $where: 'this.scheduledNotifications.REMIND.lastNotified < " +
 			"new Date(new Date().setDate(new Date().getDate() - this.scheduledNotifications.REMIND.frequency ))' }] }")
 	List<Recipient> findReadyForRemind();
 
