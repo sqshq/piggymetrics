edf097aa56a777604cc57bf1c9ad927f8e981bdd
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-08-30 16:03:58 +0500

    Updated Deprecated Mongo Bean

--- notification-service/src/main/java/com/piggymetrics/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/piggymetrics/notification/NotificationServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -23,17 +23,17 @@ import java.util.Arrays;
 @EnableScheduling
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
-	@Configuration
-	static class CustomConversionsConfig {
+    @Configuration
+    static class CustomConversionsConfig {
 
-		@Bean
-		public CustomConversions customConversions() {
-			return new CustomConversions(Arrays.asList(new FrequencyReaderConverter(),
-					new FrequencyWriterConverter()));
-		}
-	}
+        @Bean
+        public MongoCustomConversions customConversions() {
+            return new MongoCustomConversions(Arrays.asList(new FrequencyReaderConverter(),
+                    new FrequencyWriterConverter()));
+        }
+    }
 }
